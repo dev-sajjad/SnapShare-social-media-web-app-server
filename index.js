2010714b580f1c0886a8ee208273fb3defd48f37
dev-sajjad <sajjadbmarpc@gmail.com>
2023-02-19 05:38:28 +0600

    cors credential added

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
