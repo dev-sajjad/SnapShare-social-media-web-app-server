d1764ab5843f91b4e2c49918925dcf1bb3a33b6b
dev-sajjad <sajjadbmarpc@gmail.com>
2023-02-19 19:12:42 +0600

    cors error resolved

--- index.js
+++ index.js
@@ -11,13 +11,8 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
