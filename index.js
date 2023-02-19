284386607fb4858972999ab9b3a4c7c532fd1e74
dev-sajjad <sajjadbmarpc@gmail.com>
2023-02-19 19:24:24 +0600

    cors origin updated

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://snapshare-social.web.app",
+  })
+);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
